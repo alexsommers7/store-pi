14ae9144e00bcf635f0440aedf2b9c0303269f5a
alexsommers7 <asommers22@hotmail.com>
2023-07-15 16:47:41 -0700

    First pass at custom hook that acts as context middleware to communicate info on the section being currently viewed

---
+++ src/app/_hooks/useCurrentlyViewing.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import { useDocsProvider } from '@/docs/_context';
+import { structure, StructureSubItem } from '@/docs/structure';
+import { useSlugPagination, PaginationSlug } from '@/docs/pagination/useSlugPagination';
+
+interface CurrentlyViewingState {
+  currentSubitem: StructureSubItem | null;
+  prevSlug: PaginationSlug | null;
+  nextSlug: PaginationSlug | null;
+  currentAnchorLabel: string | null;
+}
+
+const defaultState: CurrentlyViewingState = {
+  currentSubitem: null,
+  prevSlug: null,
+  nextSlug: null,
+  currentAnchorLabel: null,
+};
+
+export function useCurrentlyViewing() {
+  const [currentlyViewing, setCurrentlyViewing] = useState<CurrentlyViewingState>(defaultState);
+  const { activeAnchor } = useDocsProvider();
+
+  const pathname = usePathname(); // e.g. /docs/introduction
+  const splitPath = pathname.split('/'); // e.g. ['docs', 'introduction']
+  const currentSlug = splitPath[splitPath.length - 1]; // e.g. 'introduction'
+  const { nextSlug, prevSlug } = useSlugPagination(currentSlug);
+
+  const currentSection = structure.find((item) => {
+    const allSlugs = item.subitems.map((subitem) => subitem.slug);
+    return allSlugs.includes(currentSlug);
+  });
+
+  const currentSubitem =
+    currentSection?.subitems.find((subitem) => subitem.slug === currentSlug) || null;
+
+  const currentAnchors = currentSubitem?.anchors;
+  const currentAnchorLabel =
+    currentAnchors?.find((anchor) => anchor.hash === activeAnchor)?.label || '';
+
+  useEffect(() => {
+    setCurrentlyViewing({
+      currentSubitem,
+      prevSlug,
+      nextSlug,
+      currentAnchorLabel,
+    });
+  }, [activeAnchor, currentSubitem, prevSlug, nextSlug, currentAnchorLabel]);
+
+  return { currentlyViewing };
+}
