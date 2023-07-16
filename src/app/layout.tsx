01bd5363b4946e66a75a9e27f48b67314a743272
alexsommers7 <asommers22@hotmail.com>
2023-07-15 17:08:54 -0700

    Extend nav and footer full-width and cap main content width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className='max-w-[90rem] mx-auto'>
+      <body>
         <Navigation />
         {children}
         <Footer />
