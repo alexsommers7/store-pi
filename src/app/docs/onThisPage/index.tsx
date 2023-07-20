75c88d9d7fbf19f58ed0fe5b674c69f773c5e030
alexsommers7 <asommers22@hotmail.com>
2023-07-19 20:18:37 -0700

    Fine-tune aside component styling and functionality

--- src/app/docs/onThisPage/index.tsx
+++ src/app/docs/onThisPage/index.tsx
@@ -9,19 +9,21 @@ export function OnThisPage() {
 
   return (
     <>
-      {currentSubitem?.anchors.length ? (
-        <aside className='hidden min-[1151px]:flex flex-col w-[280px] sticky top-[70px] p-6 overflow-y-auto h-full scrollbar-thin max-h-minus-nav text-left'>
-          <h3 className='font-bold tracki uppercase text-white pb-3'>On this page</h3>
+      <aside className='hidden min-[1151px]:flex flex-col w-[280px] sticky top-[70px] p-6 overflow-y-auto h-full scrollbar-thin max-h-minus-nav text-left'>
+        {currentSubitem?.anchors.length ? (
+          <>
+            <h3 className='font-bold tracki uppercase text-white pb-3'>On this page</h3>
 
-          <ul className='flex flex-col space-y-1'>
-            {currentSubitem?.anchors.map(({ label, hash }) => (
-              <li key={hash}>
-                <OnThisPageLink name={label} href={`#${hash}`} />
-              </li>
-            ))}
-          </ul>
-        </aside>
-      ) : null}
+            <ul className='flex flex-col space-y-2'>
+              {currentSubitem?.anchors.map(({ label, hash }) => (
+                <li key={hash}>
+                  <OnThisPageLink name={label} href={`#${hash}`} />
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : null}
+      </aside>
     </>
   );
 }
--- src/app/docs/sidebar/section.tsx
+++ src/app/docs/sidebar/section.tsx
@@ -1,28 +1,37 @@
 import { NavLink } from '@/_components/navigation/navLink';
-import { StructureSubItem } from '@/docs/structure';
+import { StructureItem } from '@/docs/structure';
+import { ExternalIcon } from '@/_components/icons/external';
 
 export interface SidebarSectionProps {
-  section: {
-    label: string;
-    subitems: StructureSubItem[];
-  };
+  section: StructureItem;
 }
 
 export function SidebarSection({ section }: SidebarSectionProps) {
   return (
     <div className='space-y-2'>
       <h2 className='font-bold tracki uppercase text-white'>{section.label}</h2>
-      <ul className='flex flex-col space-y-1'>
-        {section.subitems.map((subitem) => (
-          <li key={subitem.slug}>
-            <NavLink
-              name={subitem.label}
-              href={`/docs/${subitem.slug}`}
-              bold={false}
-              addSidebarAccent
-            />
-          </li>
-        ))}
+      <ul className='flex flex-col space-y-2'>
+        {section.subitems.map(({ slug, label }) => {
+          const isExternal = slug.startsWith('https');
+
+          return (
+            <li key={slug} className='flex items-center'>
+              <NavLink
+                name={label}
+                href={isExternal ? slug : `/docs/${slug}`}
+                target={isExternal ? '_blank' : '_self'}
+                bold={false}
+                addSidebarAccent
+              />
+
+              {isExternal && (
+                <span className='ml-1'>
+                  <ExternalIcon />
+                </span>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
