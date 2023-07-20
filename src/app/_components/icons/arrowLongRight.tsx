ad73d0052479738e8cef3e87b7a2ddf568ef0762
alexsommers7 <asommers22@hotmail.com>
2023-07-19 20:15:22 -0700

    Create right arrow and external icons

---
+++ src/app/_components/icons/arrowLongRight.tsx
@@ -0,0 +1,17 @@
+export function ArrowLongRightIcon() {
+  return (
+    <svg
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6 stroke-indigo-300'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
+      />
+    </svg>
+  );
+}
