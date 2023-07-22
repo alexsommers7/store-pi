7686a8dab6c3efde56274114fb4a5d4bbdc4d2b8
alexsommers7 <asommers22@hotmail.com>
2023-07-22 13:03:26 -0700

    Update id structure in quick start endpoints

--- src/app/docs/(subsections)/(getting-started)/quick-start/page.tsx
+++ src/app/docs/(subsections)/(getting-started)/quick-start/page.tsx
@@ -19,8 +19,8 @@ export default function Introduction() {
       <p className='text-left'>
         Get products within a category:{' '}
         <TextLink
-          href={`${apiOrigin}/categories/6213d49cb7f67e0d64774e93/products`}
-          label={`${apiOrigin}/categories/6213d49cb7f67e0d64774e93/products`}
+          href={`${apiOrigin}/categories/1/products`}
+          label={`${apiOrigin}/categories/1/products`}
           newTab
         />
       </p>
@@ -28,8 +28,8 @@ export default function Introduction() {
       <p className='text-left'>
         Get a specific product&apos;s reviews{' '}
         <TextLink
-          href={`${apiOrigin}/products/6213d55cf8b9765ec0fbbcaa/reviews`}
-          label={`${apiOrigin}/products/6213d55cf8b9765ec0fbbcaa/reviews`}
+          href={`${apiOrigin}/products/25/reviews`}
+          label={`${apiOrigin}/products/25/reviews`}
           newTab
         />
       </p>
