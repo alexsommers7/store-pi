448b009767fc29491074c0f20d89cd25c01bf6a3
alexsommers7 <asommers22@hotmail.com>
2023-07-29 17:08:34 -0700

    Support current user read actions and foreign table joins in sub-resource handler

--- src/app/api/v1/[resource]/[resource_id]/[sub_resource]/route.ts
+++ src/app/api/v1/[resource]/[resource_id]/[sub_resource]/route.ts
@@ -10,18 +10,56 @@ interface ColumnMap {
 const columnMap: ColumnMap = {
   categories: 'category_id',
   products: 'product_id',
+  reviews: 'review_id',
+  users: 'user_id',
 };
+
+const resourcesWithForeignTables = [
+  { resource: 'purchases', foreignTable: 'purchase_products', column: 'purchase_id' },
+  { resource: 'carts', foreignTable: 'cart_products', column: 'cart_id' },
+  { resource: 'wishlists', foreignTable: 'wishlist_products', column: 'wishlist_id' },
+];
 
 export async function GET(request: Request, context: Context) {
   try {
     const { params } = context;
-    const { resource, resource_id, sub_resource } = params; // categories, 1, products
+    const { resource, resource_id, sub_resource } = params; // e.g. { 'categories', 1, 'products' }
     const { searchParams } = new URL(request.url);
+    const isAccessingCurrentUser = resource === 'users' && resource_id === 'current';
+    const isAccessingJoinedResource = resourcesWithForeignTables.some(
+      ({ resource }) => resource === sub_resource
+    );
 
-    let query = supabase
-      .from(sub_resource)
-      .select(searchParams.get('fields') || '*', { count: 'planned' })
-      .eq(columnMap[resource], resource_id);
+    // handle foreign table querying
+    let selection = searchParams.get('select') || '*';
+    if (isAccessingJoinedResource) {
+      const curResource = resourcesWithForeignTables.find(
+        ({ resource }) => resource === sub_resource
+      );
+      if (curResource) {
+        selection = `${selection}, ${curResource.foreignTable}(*)`;
+      }
+    }
+
+    let query = supabase.from(sub_resource).select(selection, { count: 'exact' });
+
+    // auth check
+    let sessionData;
+    if (isAccessingCurrentUser) {
+      const { data } = await supabase.auth.getSession();
+      sessionData = data.session;
+
+      if (!sessionData || !sessionData.user) {
+        return NextResponse.json({ error: 'Not logged in.' }, { status: 401 });
+      }
+    }
+
+    // if not authenticated properly, return all rows
+    // RLS will handle the exclusion of more personal data like wishlists, purchases, and carts at the DB level
+    query =
+      isAccessingCurrentUser && sessionData && sessionData.user
+        ? query.eq(columnMap.users, sessionData.user.id)
+        : query.eq(columnMap[resource], resource_id);
 
     query = addFeaturesToQuery(query, searchParams);
 
@@ -29,12 +67,18 @@ export async function GET(request: Request, context: Context) {
 
     if (error || !data) {
       return NextResponse.json(
-        { error: `Failed to fetch id ${sub_resource} from ${resource} with id ${resource_id}.` },
+        {
+          error: `Failed to fetch from ${resource} with id ${resource_id}.`,
+          message: error.message,
+        },
         { status: 400 }
       );
     }
 
-    return NextResponse.json({ count, data });
+    return NextResponse.json({
+      count,
+      data,
+    });
   } catch (error) {
     return new NextResponse('An unexpected error occurred.', { status: 500 });
   }
