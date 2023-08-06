25984cc6827b0afedc88f91c03222da4ec05d125
alexsommers7 <asommers22@hotmail.com>
2023-08-06 09:03:58 -0700

    Set Access-Control-Allow-Headers response header

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,7 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   res.headers.set('Access-Control-Allow-Origin', '*');
   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.headers.set('Access-Control-Allow-Credentials', 'true');
 
   // auth
