3479aea391eb6b20024b0721692eb623eba14b90
alexsommers7 <asommers22@hotmail.com>
2023-08-05 20:02:07 -0700

    Move foreign table declarations to constants file, along with a few other declarations

--- src/app/_lib/constants.ts
+++ src/app/_lib/constants.ts
@@ -1,3 +1,5 @@
+import { Map } from '@/_lib/types';
+
 export const apiOrigin =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api/v1'
@@ -12,3 +14,48 @@ export const HTTPMethods = {
 
 export const defaultUserPhotoUrl =
   'https://storepi-media.s3.us-west-1.amazonaws.com/img/users/default.png';
+
+export const resourcesWithForeignTables = [
+  {
+    resourceName: 'purchases',
+    foreignTable: 'purchase_products',
+    column: 'purchase_id',
+    foreignTableColumns:
+      'product_id, product_data:products(id, sku, category_id, images, brand_id, name)',
+  },
+  {
+    resourceName: 'carts',
+    foreignTable: 'cart_products',
+    column: 'cart_id',
+    foreignTableColumns:
+      'product_id, quantity, product_data:products(id, sku, category_id, images, brand_id, name, regular_price, sale_price)',
+  },
+  {
+    resourceName: 'wishlists',
+    foreignTable: 'wishlist_products',
+    column: 'wishlist_id',
+    foreignTableColumns:
+      'product_id, product_data:products(id, sku, category_id, images, brand_id, name, regular_price, sale_price)',
+  },
+];
+
+// at the db level, these resources have public read access
+// but we want to limit the results to the current user's rows when they're accessing it here
+export const publicAndPrivateRead = ['reviews'];
+
+// for each resource, need to know the foreign table name to insert the new row into
+export const foreignTableMap: Map = {
+  carts: 'cart_products',
+  wishlists: 'wishlist_products',
+};
+
+// for sub-resources e.g. categories/1/products
+// in this example, would need to know to use 'category_id' as the foreign key in the products table
+export const columnMap: Map = {
+  categories: 'category_id',
+  products: 'product_id',
+  reviews: 'review_id',
+  users: 'user_id',
+  brands: 'brand_id',
+  purchases: 'purchase_id',
+};
--- src/app/_supabase/functions.ts
+++ src/app/_supabase/functions.ts
@@ -1,46 +1,26 @@
 import { cache } from 'react';
 import supabase from '@/_supabase/create-client';
+import { resourcesWithForeignTables } from '@/_lib/constants';
+import { Product } from '@/_lib/types';
 
 export const getColumnsWithType = cache(async (table: string) => {
   return await supabase.rpc('get_cols_with_type', { tname: table });
 });
 
-export async function getSessionData() {
+export async function getUserData() {
   const { data } = await supabase.auth.getSession();
-  return data.session;
+  if (!data.session || !data.session.user) return null;
+  return data.session.user;
 }
-
-const resourcesWithForeignTables = [
-  {
-    resourceName: 'purchases',
-    foreignTable: 'purchase_products',
-    column: 'purchase_id',
-    foreignTableColumns: 'product_id, products(id, sku, category_id, images, brand, name)',
-  },
-  {
-    resourceName: 'carts',
-    foreignTable: 'cart_products',
-    column: 'cart_id',
-    foreignTableColumns:
-      'product_id, products(id, sku, category_id, images, brand, name, regular_price, sale_price)',
-  },
-  {
-    resourceName: 'wishlists',
-    foreignTable: 'wishlist_products',
-    column: 'wishlist_id',
-    foreignTableColumns:
-      'product_id, products(id, sku, category_id, images, brand, name, regular_price, sale_price)',
-  },
-];
 
 export const generateForeignTableSelectionWhenApplicable = (
   resource: string,
-  searchParams: URLSearchParams
+  searchParams?: URLSearchParams
 ) => {
   const isAccessingJoinedResource = resourcesWithForeignTables.some(
     ({ resourceName }) => resourceName === resource
   );
-  let selection = searchParams.get('fields') || '*';
+  let selection = searchParams?.get('fields') || '*';
 
   if (isAccessingJoinedResource) {
     const curResource = resourcesWithForeignTables.find(
@@ -52,4 +32,42 @@ export const generateForeignTableSelectionWhenApplicable = (
   }
 
   return selection;
+};
+
+// check if a given date indicates a resource that was part of the default dataset
+export const isOriginalResource = (createdAt: string | null) => {
+  if (!createdAt) return false;
+  return new Date(createdAt) < new Date(1690936726017);
+};
+
+// these resources are plural in the db, but are represented as singular in the api for semantics
+export const addPluralityWhenApplicable = (resource: string) => {
+  const singularResources = ['cart', 'wishlist'];
+
+  if (singularResources.includes(resource)) return `${resource}s`;
+  return resource;
+};
+
+export const calculateOrderTotal = async (requestBody: any) => {
+  if (!requestBody.products) return 0;
+
+  const { data: productPricingData, error: productPricingError } = await supabase
+    .from('products')
+    .select('id, sale_price')
+    .in(
+      'id',
+      requestBody.products.map((product: Product) => product.product_id)
+    );
+
+  if (productPricingError) return 0;
+
+  const total = productPricingData.reduce((acc: number, product: any) => {
+    return (
+      acc +
+      product.sale_price *
+        requestBody.products.find((p: Product) => p.product_id === product.id).quantity
+    );
+  }, 0);
+
+  return total;
 };
