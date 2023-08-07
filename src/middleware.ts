eae60d7db91c8f4a178e39ed0691bcf62300275e
alexsommers7 <asommers22@hotmail.com>
2023-08-06 19:10:35 -0700

    Add headers to preflight response

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,18 @@ export const config = {
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
+  // pre-flight
   if (req.method === 'OPTIONS') {
-    return new NextResponse(null, { status: 200 });
+    return new NextResponse(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+      },
+    });
   }
 
   // enable cors
