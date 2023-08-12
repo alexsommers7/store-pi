6504e9e2d2718210b98bb089c2fb9f91c9faec40
alexsommers7 <asommers22@hotmail.com>
2023-08-11 21:04:38 -0700

    Small styling adjustment - vertical margin

--- src/app/_components/typography/note.tsx
+++ src/app/_components/typography/note.tsx
@@ -5,7 +5,7 @@ interface NoteProps {
 
 export function Note({ noteText, showNote = true }: NoteProps) {
   return (
-    <p className='p-4 my-8 max-w-full rounded-sm bg-indigo-400/10 border-l-2 border-indigo-300 w-max'>
+    <p className='p-4 mt-8 max-w-full rounded-sm bg-indigo-400/10 border-l-2 border-indigo-300 w-max'>
       {showNote && <strong className='text-indigo-300'>NOTE: </strong>}
       {noteText}
     </p>
