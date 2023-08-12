a716d1be4e357cb1733eaba851c8496ce92706b8
alexsommers7 <asommers22@hotmail.com>
2023-08-11 20:51:26 -0700

    Move api error handlers to dedicated file. Create new postgrest fetch utility to interact with supabase auto-generated REST api

---
+++ src/app/_utils/api-errors.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+
+// for a more user-friendly error message on RLS violations
+export function apiError(error: any) {
+  if (error.message && error.message.includes('row-level security')) {
+    return NextResponse.json(
+      { error: 'You must be logged in to perform this action.' },
+      { status: 401 }
+    );
+  } else if (typeof error === 'string') {
+    return NextResponse.json({ error }, { status: 400 });
+  }
+
+  console.log('apiError: ', error);
+
+  return NextResponse.json({ error: error?.message || 'Something went wrong.', status: 400 });
+}
+
+export function authorizationError() {
+  return NextResponse.json(
+    { error: 'You must be logged in to perform this action.' },
+    { status: 401 }
+  );
+}
+
+export function modifiedOriginalResourceError() {
+  return NextResponse.json(
+    {
+      message: `You attempted to modify an original resourec. Your data won't be persisted, but here's a 200 response for your troubles.`,
+    },
+    { status: 200 }
+  );
+}
+
+export function catchError(error: any) {
+  console.log('catchError: ', error);
+
+  const json = error instanceof Error ? error.message : 'An unexpected error occurred.';
+  return NextResponse.json({ error: json }, { status: 500 });
+}
--- src/app/_utils/rest-handlers.ts
+++ src/app/_utils/rest-handlers.ts
@@ -1,5 +1,7 @@
-import { addFeaturesToQuery } from '@/_utils/api-features';
-import { NextResponse } from 'next/server';
+import { generateQueryString } from '@/_utils/api-features';
+import { HTTPMethods, restUrlBase } from '@/_lib/constants';
+import { cookies, headers } from 'next/headers';
+import { User, createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
 interface ResponseJson {
   data: any;
@@ -7,65 +9,147 @@ interface ResponseJson {
   nextOffset?: number | null;
 }
 
-// for a more user-friendly error message on RLS violations
-export function apiError(error: any) {
-  if (error.message && error.message.includes('row-level security')) {
-    return NextResponse.json(
-      { error: 'You must be logged in to perform this action.' },
-      { status: 401 }
-    );
-  } else if (typeof error === 'string') {
-    return NextResponse.json({ error }, { status: 400 });
+interface FetchOptions {
+  method: keyof typeof HTTPMethods;
+  headers: {
+    'Content-Type'?: string;
+    apikey: string;
+    Authorization?: string;
+    Prefer?: string;
+  };
+  body?: BodyInit;
+}
+
+interface PostgrestFetchParams {
+  resource: string;
+  jwt?: string;
+  searchParams?: URLSearchParams;
+  userId?: string;
+  method?: keyof typeof HTTPMethods;
+  body?: BodyInit | { [key: string]: any };
+}
+
+interface GetUserFromRequestReturn {
+  user: User | null;
+  jwt: string | null;
+}
+
+// Postgrest
+const anonHeaders = {
+  apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  'Content-Type': 'application/json',
+  Prefer: 'count=exact',
+};
+
+const generateAuthHeaders = (jwt: string, method?: keyof typeof HTTPMethods) => {
+  const obj = {
+    ...anonHeaders,
+    Authorization: `Bearer ${jwt}`,
+  };
+
+  if (['POST', 'PATCH'].includes(method?.toUpperCase() || '')) {
+    obj.Prefer = 'return=representation';
   }
 
-  return NextResponse.json({ error: error?.message || 'Something went wrong.', status: 400 });
-}
+  return obj;
+};
 
-export function authorizationError() {
-  return NextResponse.json(
-    { error: 'You must be logged in to perform this action.' },
-    { status: 401 }
-  );
-}
+export const postgrestFetch = async ({
+  resource,
+  jwt,
+  searchParams,
+  userId,
+  body,
+  method = 'GET',
+}: PostgrestFetchParams) => {
+  try {
+    const headers = jwt ? generateAuthHeaders(jwt, method) : anonHeaders;
+    const options: FetchOptions = { method, headers };
 
-export function modifiedOriginalResourceError() {
-  return NextResponse.json(
-    {
-      message: `You attempted to modify an original resourec. Your data won't be persisted, but here's a 200 response for your troubles.`,
-    },
-    { status: 200 }
-  );
-}
-
-export function catchError(error: any) {
-  const json = error instanceof Error ? error.message : 'An unexpected error occurred.';
-  return NextResponse.json({ error: json }, { status: 500 });
-}
-
-export async function supabaseGetWithFeatures(
-  query: any,
-  searchParams: URLSearchParams,
-  isSingle: boolean = false
-) {
-  query = addFeaturesToQuery(query, searchParams);
-
-  const { data, error, count } = await query;
-
-  if (error) return { error: error.message };
-
-  const responseJson: ResponseJson = { data };
-
-  if (!isSingle) responseJson.count = count;
-
-  if (responseJson.data.length && (searchParams.get('limit') || searchParams.get('offset'))) {
-    responseJson.nextOffset =
-      Number(searchParams.get('offset')) + Number(searchParams.get('limit'));
-
-    // if at the end of pagination ...
-    if (count && responseJson.nextOffset && responseJson.nextOffset >= count) {
-      responseJson.nextOffset = null;
+    if (body && !['GET', 'DELETE'].includes(method.toUpperCase())) {
+      options.body = JSON.stringify(body);
     }
-  }
 
-  return responseJson;
-}
+    const queryString = generateQueryString(userId, resource, searchParams, method === 'GET');
+
+    const res = await fetch(
+      `${restUrlBase}/${resource}${queryString ? '?' + queryString : ''}`,
+      options
+    );
+
+    if (!res.ok) throw new Error(res.statusText);
+
+    let json;
+
+    if (method === 'DELETE') {
+      return { json: null, error: null };
+    }
+
+    if (['POST', 'PATCH'].includes(method.toUpperCase())) {
+      json = await res.json();
+      return { json, error: null };
+    }
+
+    json = await res.json();
+
+    const contentRange = res.headers.get('content-range');
+    const count = contentRange ? Number(contentRange.split('/')[1]) : null;
+
+    const responseJson: ResponseJson = { data: json, count };
+
+    if (responseJson.data.length && (searchParams?.get('limit') || searchParams?.get('offset'))) {
+      responseJson.nextOffset =
+        Number(searchParams.get('offset')) + Number(searchParams.get('limit'));
+
+      // if at the end of pagination ...
+      if (count && responseJson.nextOffset && responseJson.nextOffset >= count) {
+        responseJson.nextOffset = null;
+      }
+    }
+
+    return { json: responseJson, error: null };
+  } catch (error) {
+    return { json: null, error: error as Error };
+  }
+};
+
+export const getUserFromRequest = async (): Promise<GetUserFromRequestReturn> => {
+  const supabase = createRouteHandlerClient({ cookies });
+
+  const headersInstance = headers();
+  const authorization = headersInstance.get('authorization');
+
+  if (!authorization) {
+    return { user: null, jwt: '' };
+  }
+  const jwt = authorization.split(' ')[1];
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser(jwt);
+
+  return { user, jwt };
+};
+
+export const callStoredProcedure = async ({
+  jwt,
+  procedureName,
+  params,
+}: {
+  jwt: string;
+  procedureName: string;
+  params: any;
+}) => {
+  try {
+    const headers = generateAuthHeaders(jwt);
+    const options: FetchOptions = { method: 'POST', headers, body: JSON.stringify(params) };
+
+    const res = await fetch(`${restUrlBase}/rpc/${procedureName}`, options);
+
+    if (!res.ok) throw new Error(res.statusText);
+
+    return await res.json();
+  } catch (error) {
+    return error;
+  }
+};
