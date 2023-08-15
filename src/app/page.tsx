b89648719f729c74c9407f28685132c27143496b
alexsommers7 <asommers22@hotmail.com>
2023-08-14 18:01:19 -0700

    Home page coming soon messaging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <main className='flex flex-col items-center justify-between p-24'>
         <h1 className='text-6xl font-bold text-center'>StorePI</h1>
-        <p className='p-3'>Welcome, this will be the home page</p>
+        <p className='p-3'>Home page coming soon!</p>
       </main>
     </>
   );
