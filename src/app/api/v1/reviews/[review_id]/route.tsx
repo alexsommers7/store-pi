62a74190983bf5120ecffbbd429cfcc7ce02bac6
alexsommers7 <asommers22@hotmail.com>
2023-08-14 17:56:43 -0700

    Add stored procedure call to review resource updates to update product stats

--- src/app/api/v1/reviews/[review_id]/route.tsx
+++ src/app/api/v1/reviews/[review_id]/route.tsx
@@ -7,7 +7,7 @@ import {
 } from '@/_utils/api-errors';
 import { Context } from '@/_lib/types';
 import { isOriginalResource } from '@/_supabase/server-functions';
-import { getUserFromRequest, postgrestFetch } from '@/_utils/rest-handlers';
+import { callStoredProcedure, getUserFromRequest, postgrestFetch } from '@/_utils/rest-handlers';
 
 export async function GET(request: Request, context: Context) {
   try {
@@ -46,7 +46,7 @@ export async function PATCH(request: Request, context: Context) {
 
     if (selectError) return apiError(selectError);
 
-    if (isOriginalResource(selectJson.data[0]?.created_at)) {
+    if (isOriginalResource(selectJson.data[0].created_at)) {
       return modifiedOriginalResourceError();
     }
 
@@ -61,6 +61,16 @@ export async function PATCH(request: Request, context: Context) {
     });
 
     if (error) return apiError(error);
+
+    const { error: storedProcedureError } = await callStoredProcedure({
+      procedureName: 'update_product_review_stats',
+      jwt,
+      params: {
+        product_id_param: selectJson.data[0].product_id,
+      },
+    });
+
+    if (storedProcedureError) return apiError(storedProcedureError);
 
     return NextResponse.json({ data: json[0] });
   } catch (error) {
@@ -94,6 +104,16 @@ export async function DELETE(request: Request, context: Context) {
       jwt,
       searchParams: new URLSearchParams({ id: review_id }),
     });
+
+    const { error: storedProcedureError } = await callStoredProcedure({
+      procedureName: 'update_product_review_stats',
+      jwt,
+      params: {
+        product_id_param: selectJson.data[0].product_id,
+      },
+    });
+
+    if (storedProcedureError) return apiError(storedProcedureError);
 
     if (error) return apiError(error);
 
--- src/app/api/v1/reviews/route.tsx
+++ src/app/api/v1/reviews/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { catchError, apiError, authorizationError } from '@/_utils/api-errors';
-import { getUserFromRequest, postgrestFetch } from '@/_utils/rest-handlers';
+import { callStoredProcedure, getUserFromRequest, postgrestFetch } from '@/_utils/rest-handlers';
 
 export async function GET(request: Request) {
   try {
@@ -32,6 +32,16 @@ export async function POST(request: Request) {
     });
 
     if (error) return apiError(error);
+
+    const { error: storedProcedureError } = await callStoredProcedure({
+      procedureName: 'update_product_review_stats',
+      jwt,
+      params: {
+        product_id_param: requestBody.product_id,
+      },
+    });
+
+    if (storedProcedureError) return apiError(storedProcedureError);
 
     return NextResponse.json({ data: json[0] });
   } catch (error) {
